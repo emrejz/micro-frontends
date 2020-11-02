06f96c08a9373ef4de67982f07fd541122e08da1
emre kayacan <emre.kayacan@msn.com>
2020-11-02 17:49:36 +0300

    header ok

--- projects/nav-bar/src/root.component.js
+++ projects/nav-bar/src/root.component.js
@@ -1,6 +1,11 @@
 import React from "react";
+import "./index.css";
 
-export default function Root(props) {
-  console.log(props);
-  return <h1>EMREEEEEEEEEEEEEE</h1>;
+export default function Root({ singleSpa: { navigateToUrl } }) {
+  return (
+    <nav className="navBarContainer">
+      <h2 onClick={() => navigateToUrl("/")}>Home</h2>
+      <h2 onClick={() => navigateToUrl("/emre")}>Emre</h2>
+    </nav>
+  );
 }
