2eb345cddebdc540b0d92471c0c333163abad706
emre kayacan <emre.kayacan@msn.com>
2020-11-29 22:30:32 +0300

    removed logs

--- projects/root/src/mf-root-config.js
+++ projects/root/src/mf-root-config.js
@@ -6,12 +6,17 @@ import {
 } from "single-spa-layout";
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
+console.log("🚀 ~ file: mf-root-config.js ~ line 9 ~ routes", routes);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+console.log(
+  "🚀 ~ file: mf-root-config.js ~ line 16 ~ applications",
+  applications
+);
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
